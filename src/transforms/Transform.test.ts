58169f0bd2af1ebda3f627f8dbbb2240d929ddc8
Jón Rúnar Helgason <dev@jonrh.is>
2020-01-12 20:51:17 +0000

    Added a unit test for the data transformations
    WIP

---
+++ src/transforms/Transform.test.ts
@@ -0,0 +1,56 @@
+import { data } from "../data";
+import {chartTransform, getUniqueRegions} from "./Transform";
+
+test("unique regions should be us, ca, unknown", () => {
+  expect(getUniqueRegions(data).sort()).toEqual(["unknown", "ca", "us"].sort());
+  expect(getUniqueRegions(data).sort()).toEqual(["us", "ca", "unknown"].sort());
+  expect(getUniqueRegions(data).sort()).toEqual(["ca", "us", "unknown"].sort());
+  expect(getUniqueRegions(data).sort()).toEqual(["ca", "unknown", "us"].sort());
+});
+
+test("chartTransform returns the expected output", () => {
+  const actual = chartTransform(data);
+  const expected = [
+    {
+      name: "unknown Views",
+      data: [
+        ["2020-01-02T00:00:00.000Z", 3743],
+        ["2020-01-03T00:00:00.000Z", 3361],
+        ["2020-01-04T00:00:00.000Z", 4254],
+        ["2020-01-05T00:00:00.000Z", 3731],
+        ["2020-01-06T00:00:00.000Z", 3147],
+        ["2020-01-07T00:00:00.000Z", 3529],
+        ["2020-01-08T00:00:00.000Z", 4346],
+        ["2020-01-09T00:00:00.000Z", 2245]
+      ]
+    },
+    {
+      name: "us Views",
+      data: [
+        ["2020-01-02T00:00:00.000Z", 326484],
+        ["2020-01-03T00:00:00.000Z", 312514],
+        ["2020-01-04T00:00:00.000Z", 345289],
+        ["2020-01-05T00:00:00.000Z", 336700],
+        ["2020-01-06T00:00:00.000Z", 293539],
+        ["2020-01-07T00:00:00.000Z", 285198],
+        ["2020-01-08T00:00:00.000Z", 291301],
+        ["2020-01-09T00:00:00.000Z", 167741]
+      ]
+    },
+    {
+      name: "ca Views",
+      data: [
+        ["2020-01-02T00:00:00.000Z", 43281],
+        ["2020-01-03T00:00:00.000Z", 41245],
+        ["2020-01-04T00:00:00.000Z", 44725],
+        ["2020-01-05T00:00:00.000Z", 47593],
+        ["2020-01-06T00:00:00.000Z", 39423],
+        ["2020-01-07T00:00:00.000Z", 37326],
+        ["2020-01-08T00:00:00.000Z", 39688],
+        ["2020-01-09T00:00:00.000Z", 23324]
+      ]
+    },
+  ];
+
+  expect(actual).toBe(expected);
+});
